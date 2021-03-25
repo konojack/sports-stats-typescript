615f83bf82181e66e48862cd41c3befaff2cd066
konojack <konojack@gmail.com>
2021-03-25 15:54:12 +0100

    Read CSV file and create array of arrays based on this file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('TEST');
+import fs from 'fs';
+
+const matches: string[][] = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+console.log(matches);
