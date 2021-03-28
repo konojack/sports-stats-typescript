0cbb05b9ef5880c83dcb23ea74965a3473afb460
konojack <konojack@gmail.com>
2021-03-28 19:53:35 +0200

    Enum MatchResult create

--- build/index.js
+++ build/index.js
@@ -12,4 +12,20 @@ var matches = fs_1.default
     .map(function (row) {
     return row.split(',');
 });
-console.log(matches);
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+var manUtdWins = 0;
+for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+    var match = matches_1[_i];
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUtdWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUtdWins++;
+    }
+}
+console.log("Man United won " + manUtdWins + " games");
