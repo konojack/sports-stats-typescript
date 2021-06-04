4f0bd8d20d50a00ec7185e9cee6d558abdcdcea6
Dawid Konojacki <dawid.konojacki@sb-betting.com>
2021-06-04 13:39:08 +0200

    Create HtmlReport and generate HTML file report.html

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,22 @@ import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { HtmlReport } from './reportTargets/HtmlReport';
 import { Summary } from './Summary';
 
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(
+const summaryHtml = new Summary(
   new WinsAnalysis('Man City'),
+  new HtmlReport()
+)
+
+const summaryConsole = new Summary(
+  new WinsAnalysis('Man United'),
   new ConsoleReport()
 )
 
-summary.buildAndPrintReport(matchReader.matches);
+summaryHtml.buildAndPrintReport(matchReader.matches);
+summaryConsole.buildAndPrintReport(matchReader.matches);
