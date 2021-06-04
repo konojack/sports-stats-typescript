eb0058e0d3ba3c7097bfc3ca55855b5785b66c08
Dawid Konojacki <dawid.konojacki@sb-betting.com>
2021-06-04 13:25:53 +0200

    Using Summary inside index.js

--- build/index.js
+++ build/index.js
@@ -1,19 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
 var CsvFileReader_1 = require("./CsvFileReader");
 var MatchReader_1 = require("./MatchReader");
-var MatchResult_1 = require("./MatchResult");
+var ConsoleReport_1 = require("./reportTargets/ConsoleReport");
+var Summary_1 = require("./Summary");
 var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 var matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
-var manUtdWins = 0;
-for (var _i = 0, _a = matchReader.matches; _i < _a.length; _i++) {
-    var match = _a[_i];
-    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
-        manUtdWins++;
-    }
-    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
-        manUtdWins++;
-    }
-}
-console.log("Man United won " + manUtdWins + " games");
+var summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man City'), new ConsoleReport_1.ConsoleReport());
+summary.buildAndPrintReport(matchReader.matches);
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { Summary } from './Summary';
 
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-let manUtdWins = 0;
+const summary = new Summary(
+  new WinsAnalysis('Man City'),
+  new ConsoleReport()
+)
 
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUtdWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUtdWins++;
-  }
-}
-console.log(`Man United won ${manUtdWins} games`);
+summary.buildAndPrintReport(matchReader.matches);
